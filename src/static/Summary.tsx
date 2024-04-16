2174e87514c2932960ea629bc6ac4d693516e019
DenGolivets <oxytechmusic@gmail.com>
2024-04-16 03:56:00 +0300

    fix apos

--- src/static/Summary.tsx
+++ src/static/Summary.tsx
@@ -6,13 +6,13 @@ const Summary = () => {
     </p>
       <article className="desktop:text-xl mobile:text-md font-poppins" itemScope itemType="https://schema.org/author">
         <p className="mb-5">
-          Hello! I'm Den Holivets, a young, promising front-end developer passionate about creating visually appealing, user-friendly websites. 
+          Hello! I&apos;m Den Holivets, a young, promising front-end developer passionate about creating visually appealing, user-friendly websites. 
           My journey in the world of web development has equipped me with a versatile skill set, including proficiency in HTML, CSS, JavaScript, 
           ReactJs, NextJs, TypeScript, MongoDB, API integration, and a touch of MERN stack development. I also took courses at the IT Institute 
           with Senior <b><a className="hover:animate-pulse" href="https://www.instagram.com/nik.kipniak"><span className="text-purple-500">Mykola Kipnyak</span></a></b>. 
         </p>
         <p className="mb-5">
-          At the moment I am participating in a startup project and studying 🎓 new technologies in the world of IT.
+          At the moment I&apos;m participating in a startup project and studying 🎓 new technologies in the world of IT.
         </p>
         <p className="mb-5">
           When I&apos;m not at the computer, I create electronic music 🎶.
