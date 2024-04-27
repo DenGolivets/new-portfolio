59e6278e7c1a151df78ac8de864685f532d483cc
DenGolivets <oxytechmusic@gmail.com>
2024-04-27 13:36:31 +0300

    add project

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,6 +1,7 @@
 const shopotube = "/images/Shopotube.jpg";
 const landing = "/images/Landing.jpg";
 const carrental = "/images/CarRental.jpg";
+const coffecafe = "/images/CoffeCafe.jpg";
 
 const data = [
     {
@@ -11,18 +12,25 @@ const data = [
       text: "A responsive e-commerce frontend for cosmetic shopping on desktop and mobile."
     },
     {
-      title: "Test Landing Page", 
-      image: landing, 
-      link: "https://collab-test-six.vercel.app", 
-      tags: ["Next JS", "Typescript", "Tailwind CSS"], 
-      text: "A single-page application(landing)."
-    },
+      title: "Coffe Cafe", 
+      image: coffecafe, 
+      link: "https://coffee-shopify.netlify.app/", 
+      tags: ["Next JS", "Javascript", "AOS"], 
+      text: "A responsive landing page of Coffe cafe"
+    },    
     {
       title: "Car Rental Landing Page", 
       image: carrental, 
       link: "https://carshop-six.vercel.app/", 
       tags: ["Next JS", "React JS", "JavaScript", "Tailwind CSS", "framer-motion"], 
       text: "A Car rental single-page with ui/ux (count-up, animated)."
+    },
+    {
+      title: "Test Landing Page", 
+      image: landing, 
+      link: "https://collab-test-six.vercel.app", 
+      tags: ["Next JS", "Typescript", "Tailwind CSS"], 
+      text: "A single-page application(landing)."
     },
 ]
 export default data;
